e637844e38e80a992e9828063659f7a66777724a
Lukas <idea8dreams@gmail.com>
2024-03-09 18:19:09 +0100

    Added functionalities for day 9: In Api layer: Cleaned up Program.cs Created: ApplicationConfigurationController, TestsController
    In Application layer:
    Created:
    ICommand, ICommandHandler, IQuery, IQueryHandler
    SomeCommandHandler, SomeCommand, SomeDto, SomeQuery
    
    Added:
    Scrutor Dependency
    Dependency Injection Scanning for ICommandHandler
    
    In Infrastructure layer:
    Created:
    ConfigProvider
    SomeQueryHandler
    
    Added Dependency Injection Scanning for IQueryHandler
    Registered repositories.

---
+++ src/backend/dotnet/Freezbe.Application/Commands/Handlers/SomeCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using Freezbe.Application.Abstractions;
+
+namespace Freezbe.Application.Commands.Handlers;
+
+public class SomeCommandHandler : ICommandHandler<SomeCommand>
+{
+    public Task HandleAsync(SomeCommand command)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/backend/dotnet/Freezbe.Infrastructure/Extensions.cs
+++ src/backend/dotnet/Freezbe.Infrastructure/Extensions.cs
@@ -1,14 +1,59 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Freezbe.Application.Abstractions;
+using Freezbe.Infrastructure.Configurations;
+using Freezbe.Infrastructure.DataAccessLayer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Freezbe.Infrastructure;
 
 public static class Extensions
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddControllers();
+        services.AddConfigurations(configuration);
+        // Learn more about configuring Swagger/OpenA   PI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddDataAccessLayer();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddQueryHandlers();
+
         //services.AddSingleton<Interface, Implementation>();
         //services.AddScoped<Interface, Implementation>();
         //services.AddTransient<Interface, Implementation>();
+        return services;
+    }
+
+    public static WebApplication UseInfrastructure(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if(app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+        return app;
+    }
+
+    private static IServiceCollection AddQueryHandlers(this IServiceCollection services)
+    {
+        var currentAssembly = typeof(ApplicationConfiguration).Assembly;
+
+        services
+        .Scan(s => s.FromAssemblies(currentAssembly)
+        .AddClasses(c => c.AssignableTo(typeof(IQueryHandler<,>)))
+        .AsImplementedInterfaces()
+        .WithScopedLifetime());
+
         return services;
     }
 }
