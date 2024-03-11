75608fed2b9078092e198d5ac0b9eb9dd5c5f5c1
Lukas <idea8dreams@gmail.com>
2024-03-11 19:01:27 +0100

    Create IProjectRepository in Core layer #24

---
+++ src/backend/dotnet/Freezbe.Core/Repositories/IProjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using Freezbe.Core.Entities;
+using Freezbe.Core.ValueObjects;
+
+namespace Freezbe.Core.Repositories;
+
+public interface IProjectRepository
+{
+    Task<Project> Get(ProjectId projectId);
+    Task<IEnumerable<Project>> GetAllAsync();
+    Task AddAsync(Project project);
+    Task Update(Project project);
+    Task DeleteAsync(Project project);
+}
