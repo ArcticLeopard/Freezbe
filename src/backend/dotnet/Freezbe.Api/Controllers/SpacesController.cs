a7523a88b4ca756437766f3447559e0e37013b33
Lukas <idea8dreams@gmail.com>
2024-03-24 17:17:28 +0100

    Create CreateSpaceCommand, CreateSpaceCommandHandler, SpacesController #69

---
+++ src/backend/dotnet/Freezbe.Api/Controllers/SpacesController.cs
@@ -0,0 +1,26 @@
+﻿using Freezbe.Api.Requests;
+using Freezbe.Application.Abstractions;
+using Freezbe.Application.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Freezbe.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SpacesController : ControllerBase
+{
+    private readonly ICommandHandler<CreateSpaceCommand> _commandHandler;
+
+    public SpacesController(ICommandHandler<CreateSpaceCommand> commandHandler)
+    {
+        _commandHandler = commandHandler;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(CreateSpaceRequest request)
+    {
+        var command = new CreateSpaceCommand(Guid.NewGuid(), request.Description);
+        await _commandHandler.HandleAsync(command);
+        return NoContent();
+    }
+}
---
+++ src/backend/dotnet/Freezbe.Application/Commands/CreateSpaceCommand.cs
@@ -0,0 +1,5 @@
+﻿using Freezbe.Application.Abstractions;
+
+namespace Freezbe.Application.Commands;
+
+public record CreateSpaceCommand(Guid Id, string Description) : ICommand;
