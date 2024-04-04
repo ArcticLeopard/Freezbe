f6014f47b7c9cfbb1ebc2f2418bf89134510a809
Lukas <idea8dreams@gmail.com>
2024-04-04 02:37:02 +0200

    Create TaskView Component in Angular #101

--- src/frontend/angular/src/app/app.routes.ts
+++ src/frontend/angular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {TaskViewComponent} from "./components/task-view/task-view.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'taskView', component: TaskViewComponent
+  }
+];
