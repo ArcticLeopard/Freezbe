fd62da882595bf78a834210c15a771174674921e
Lukas <idea8dreams@gmail.com>
2024-04-20 23:18:25 +0200

    Create User Interface - Step 4 #148

--- src/frontend/angular/src/app/components/detail-list.component.ts
+++ src/frontend/angular/src/app/components/detail-list.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-import {NgForOf} from "@angular/common";
+import {NgClass, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-detail-list',
   standalone: true,
-    imports: [
-        NgForOf
-    ],
+  imports: [
+    NgForOf,
+    NgClass
+  ],
   templateUrl: './detail-list.component.html',
   styleUrl: './detail-list.component.scss'
 })
 export class DetailListComponent {
-  commentIndexes = Array(25).fill(0).map((x, i) => i);
+  colors: string[] = ['blue','indigo','purple','pink','red','orange','yellow','green','teal','cyan','white','gray','gray-dark','primary','secondary','success','info','warning','danger','light','dark','classic']
 }
+
+
