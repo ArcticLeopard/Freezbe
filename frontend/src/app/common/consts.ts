ccef6f94ae433a32bf44a51fb099ac815905e78d
Lukas <idea8dreams@gmail.com>
2024-07-03 23:10:42 +0200

    Day 125 Updates

--- frontend/src/app/common/consts.ts
+++ frontend/src/app/common/consts.ts
@@ -1,28 +1,56 @@
-export const workspace = "workspace";
-export const workspaceId = "workspaceId";
-export const workspaces = "workspaces";
+export const workspace = 'workspace';
+export const workspaceId = 'workspaceId';
+export const workspaces = 'workspaces';
 
-export const project = "project";
-export const projectId = "projectId";
-export const projects = "projects";
+export const project = 'project';
+export const projectId = 'projectId';
+export const projects = 'projects';
 
-export const task = "task";
-export const taskId = "taskId";
-export const tasks = "tasks";
+export const task = 'task';
+export const taskId = 'taskId';
+export const tasks = 'tasks';
 
-export const details = "details";
-export const comment = "comment";
-export const commentId = "commentId";
-export const comments = "comments";
+export const details = 'details';
 
-export const view = "view";
-export const priority = "priority";
-export const incoming = "incoming";
-export const singleTasks = "single_tasks";
-export const calendar = "calendar";
+export const priority = 'priority';
+export const incoming = 'incoming';
+export const singleTasks = 'single_tasks';
+export const calendar = 'calendar';
 
-export const colors: string[] = ['#007bff', '#6610f2', '#6f42c1', '#e83e8c', '#dc3545', '#fd7e14', '#ffc107', '#28a745', '#20c997', '#17a2b8'];
+export const colors: string[] = [
+  '#007bff',
+  '#6610f2',
+  '#6f42c1',
+  '#e83e8c',
+  '#dc3545',
+  '#fd7e14',
+  '#ffc107',
+  '#28a745',
+  '#20c997',
+  '#17a2b8',
+];
 
-export const daysCollection: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+export const daysCollection: string[] = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
 
-export const monthsCollection: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+export const monthsCollection: string[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
